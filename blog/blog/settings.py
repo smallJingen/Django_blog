113ccddbf960753c598721a37e06a465a55ee2f0
hjg <hjg@zhengjing.com>
2022-04-27 13:50:44 +0800

    mysql配置好了

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -74,10 +74,22 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+        'ENGINE':'django.db.backends.mysql',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+        'USER':'itblog',
+        'PASSWORD':'20001116',
+        'NAME':'blog'
     }
 }
 
